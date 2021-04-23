398899dadf4c626dbbd22398dc0edb560151fe7c
Toxic Dev <59587139+TheRealToxicDev@users.noreply.github.com>
2021-04-23 13:00:16 -0600

    Create index.js

---
+++ index.js
@@ -0,0 +1,34 @@
+const fetch = require("node-fetch");
+
+/**
+* FATES LIST API WRAPPER
+*
+* Made with ❤️ By Toxic Dev 
+*/
+
+class FL {
+    constructor(id, auth){
+        this.id = id
+        this.auth = auth
+    }
+    async post(server_count, shard_count){
+        let body = shard_count ? { 'guild_count': server_count, 'shard_count': shard_count } : { 'guild_count': server_count, 'shard_count': 0 }
+        await fetch(`https://fateslist.xyz/api/bots/${this.id}/stats`, {
+            method: 'POST',
+            body:    JSON.stringify(body),
+            headers: { 'Content-Type': 'application/json', 'authorization': this.auth },
+        }).then(async (res) => {console.log(await res.json())})
+    }
+
+    async get(botID, response, compact){
+        if(!botID) throw new Error("Missing Bot ID, Should be String")
+        fetch(`https://fateslist.xyz/api/bots/${botID}` + (compact ? '?compact=true' : ''), {
+            method: "GET",
+            headers: {"Content-Type": "application/json"},
+        }).then(async res => {
+            response(await res.json())
+        })
+    }
+}
+
+module.exports = FL;
